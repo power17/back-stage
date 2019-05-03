bad890c034954ef211c336d57e0623c3a7974d89
power <1410837981@qq.com>
2019-05-03 22:45:39 +0800

    后台样式的搭建

---
+++ src/components/common/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-header height="70px">我是头部</el-header>  
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.el-header {
+    background-color: #242f42;
+    color: #333;
+    line-height: 70px;
+  }
+</style>
---
+++ src/components/common/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<!-- <div>
+<el-container style="height: 500px; border: 1px solid #eee">  
+        <v-header></v-header>
+      
+            <v-asider></v-asider>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+            
+        
+        
+    </el-container>
+</div> -->
+<el-contaier class="wrapper">
+    <v-header></v-header>
+    <el-container style="height: 500px; border: 1px solid #eee">
+        <v-asider></v-asider>
+        <router-view></router-view>
+    </el-container>
+</el-contaier>
+
+    
+</template>
+<script>
+import vHeader from './Header'
+import vAsider from './Asider'
+
+
+export default {
+    components:{
+        vHeader,
+        vAsider
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+  
+  .el-aside {
+    color: #333;
+  }
+  .el-container{
+      height: 100%!important;
+  }
+
+</style>
